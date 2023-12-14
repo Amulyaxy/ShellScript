42008da9fce4ee51e081624100bb021a02400b99
unknown <amulyasreeram@gmail.com>
2023-12-14 10:34:00 +0530

    added 2nd script

---
+++ 06-Array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FRUITS=("Apple","Banana","Grapes")
+
+echo "First value : ${FRUITS[0]}"
+echo "Second value : ${FRUITS[1]}"
+echo "Third value : ${FRUITS[2]}"
+ech0 "All Fruits : ${FRUITS[@]}"
