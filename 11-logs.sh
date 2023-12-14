3c9df2da0aec7a7831a61474431bac0e6dae8966
unknown <amulyasreeram@gmail.com>
2023-12-14 22:40:55 +0530

    added 2nd script

---
+++ 11-logs.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+
+ID=($id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S) 
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "ERROR:: $2...... failed"
+        exit 1
+    else 
+        echo "$2..... is success"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: you are not in to the root user"
+    exit 1
+else
+    echo "you are in root user"
+fi
+
+yum install mysql -y &>> $LOGFILE
+
+VALIDATE $? "Installing MYSQL"
+
+yum install git -y &>> $LOGFILE
+
+VALIDATE $? "Installing Git"
