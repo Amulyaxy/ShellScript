53763ad22f9e0c717403a3b300406908b4501b25
unknown <amulyasreeram@gmail.com>
2023-12-14 22:20:23 +0530

    added 2nd script

--- 12-datatypes.sh
+++ 12-datatypes.sh
@@ -3,9 +3,9 @@
 NUMBER=$1
 NUMBER=$2
 
-SUM=$(($NUMBER1 + $NUMBER2))
+SUM=$(($NUMBER1+$NUMBER2))
 
-echo "Total::$SUM"
+echo "Total:: $SUM"
 
 echo "How many args passed:: $#"
 echo "All args passed:: $@"
