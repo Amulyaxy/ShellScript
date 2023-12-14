f8f7dae643c35a59ebece19feee9148dff2d65f2
unknown <amulyasreeram@gmail.com>
2023-12-14 19:48:39 +0530

    added 2nd script

---
+++ 10-functions.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+ID=($id -u) 
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+        echo "ERROR:: installation failed"
+        exit 1
+    else 
+        echo "Installation is success"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: you are not in to the root user"
+    exit 1
+else
+    echo "you are in root user"
+fi
+
+yum install mysql -y
+
+VALIDATE
+
+yum install git -y
