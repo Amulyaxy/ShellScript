5f2ffa2717a555f2b201e7edb602b87fbe5128bb
unknown <amulyasreeram@gmail.com>
2023-12-14 23:04:58 +0530

    added 2nd script

--- 11-logs.sh
+++ 11-logs.sh
@@ -13,19 +13,19 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo e "ERROR:: $2...... $R failed $N installing"
+        echo -e "ERROR:: $2...... $R failed $N installing"
         exit 1
     else 
-        echo  e "$2..... is $G success"
+        echo -e "$2..... is $G success"
     fi
 }
 
 if [ $ID -ne 0 ]
 then
-    echo e "$R ERROR:: you are not in to the root user $N"
+    echo -e "$R ERROR:: you are not in to the root user $N"
     exit 1
 else
-    echo e "$G you are in root user $N"
+    echo -e "$G you are in root user $N"
 fi
 
 yum install mysql -y &>> $LOGFILE
