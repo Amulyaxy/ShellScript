7ed6812527ab950eebd5a43dde14302e2478f1c1
unknown <amulyasreeram@gmail.com>
2023-12-19 22:36:25 +0530

    delete old logs

---
+++ roboshop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+AMI=ami-03265a0778a880afb
+SECURITYGROUP_ID=sg-0abbe9ab46109976d
+#REGION=us-east-1
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "cart" "catalogue" "user" "web" "shipping" "payment" "dispatch")
+
+for i in "${INSTANCES[@]}"
+do
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+
+    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids  sg-0abbe9ab46109976d
+    
+done
+
+
