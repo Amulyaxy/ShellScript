5340309954efcb120c9a2b87b48dc81a88e79209
unknown <amulyasreeram@gmail.com>
2023-12-19 18:16:47 +0530

    delete old logs

---
+++ 16-disk-usage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
+DISK_THRESHOLD=1
+message=""
+
+while IFS= read line
+do  
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)|
+    partition=$(echo $line | aws '{print $1F}')
+    if [$usage -gt $Disl_THRESHOLD]
+    then
+        message+="High disk usage on $partition: $usage"
+done <<< $DISK_USAGE
