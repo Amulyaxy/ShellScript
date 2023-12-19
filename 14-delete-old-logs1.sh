5f1af03cb1d1be78dc80a4017973e2b8d62b21b9
unknown <amulyasreeram@gmail.com>
2023-12-19 16:07:20 +0530

    delete old logs

--- 14-delete-old-logs1.sh
+++ 14-delete-old-logs1.sh
@@ -2,6 +2,12 @@
 
 SOURCE_DIR="tmp/shellscript-logs"
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
+
 if [ ! -d $SOURCE_DIR ]
 then
     echo -e "$R Source directory: $SOURCE_DIR does not exists. $N"
