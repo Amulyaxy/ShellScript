f842bba7aa963176674362f31ae5384d56fad004
unknown <amulyasreeram@gmail.com>
2023-12-19 19:13:25 +0530

    delete old logs

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -7,9 +7,11 @@ message=""
 while IFS= read line
 do  
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)|
-    partition=$(echo $line | aws '{print $1F}');
-    if [[ $usage -ge $DISK_THRESHOLD ]]
+    partition=$(echo $line | awk '{print $1F}')
+    if [ $usage -ge $DISK_THRESHOLD ]
     then
         message="High disk usage on $partition: $usage"
     fi
 done <<< $DISK_USAGE
+ 
+ echo "Message: $message"
