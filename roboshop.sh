832126955cf5eab0a034c1578d03a10b663064cd
unknown <amulyasreeram@gmail.com>
2023-12-19 22:37:21 +0530

    delete old logs

--- roboshop.sh
+++ roboshop.sh
@@ -14,8 +14,8 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids  sg-0abbe9ab46109976d
-    
+    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids  sg-0abbe9ab46109976d
+
 done
 
 
