37b13e41eba8875d647ecd99e109c19f84f18289
unknown <amulyasreeram@gmail.com>
2023-12-19 16:19:57 +0530

    delete old logs

--- 14-delete-old-logs1.sh
+++ 14-delete-old-logs1.sh
@@ -13,7 +13,7 @@ then
     echo -e "$R Source directory: $SOURCE_DIR does not exists. $N"
 fi
 
-FILES_TO_DELETE=$(find . -type f -mtime +7 -name "*.log")
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +7 -name "*.log")
 
 while IFS= read -r line
 do 
